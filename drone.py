68841c01b40003a760a699460e8e2a7bfd9360d3
AidenNeale <aidenneale@sky.com>
2023-07-03 23:24:46 +0100

    Added functionality for drone to capture image post-movement

--- drone.py
+++ drone.py
@@ -1,4 +1,4 @@
-import time
+import cv2, time
 
 import robomaster
 from robomaster import robot
@@ -17,7 +17,10 @@ class Drone():
         self.flight = self.drone.flight
         self.camera = self.drone.camera
 
-    def camera_capture(self, display_on_screen=False):
+        self.current_frame_id = 0
+
+
+    def enable_camera(self, display_on_screen=False):
         self.camera.start_video_stream(display=display_on_screen)
 
 
@@ -27,31 +30,57 @@ class Drone():
         self.drone.close()
 
 
+    def frame_capture(self):
+        time.sleep(1.5) # Sleep to stabilise drone before image capture
+        img = self.camera.read_cv2_image()
+        filepath = 'RoboMaster_Img_' + self.current_frame_id
+        self.current_frame_id += 1 # Increment ID of Frame
+        cv2.imwrite(filepath, img)
+
+
     def takeoff(self):
         self.flight.takeoff().wait_for_completed()
+        self.flight.up(distance=50).wait_for_completed()  
+
 
     def land(self):
         self.flight.land().wait_for_completed()
 
 
-    def move_forward(self, position):
+    def head_to_start(self):
+        self.move_forward(position=200)
+        self.move_left(position=200)
+
+
+    def return_home(self):
+        self.move_backward(position=400)
+        self.move_left(position=300)
+        self.move_backward(position=100)
+
+
+    def move_forward(self, position=0, capture_frame=False):
         self.flight.forward(distance=position).wait_for_completed()
+        if capture_frame:
+            self.frame_capture()
 
 
-    def move_backward(self, position):
+    def move_backward(self, position=0, capture_frame=False):
         self.flight.backward(distance=position).wait_for_completed()
+        if capture_frame:
+            self.frame_capture()
 
 
-    def move_left(self, position):
+    def move_left(self, position=0, capture_frame=False):
         self.flight.left(distance=position).wait_for_completed()
+        if capture_frame:
+            self.frame_capture()
 
 
-    def move_right(self, position):
+    def move_right(self, position=0, capture_frame=False):
         self.flight.right(distance=position).wait_for_completed()
+        if capture_frame:
+            self.frame_capture()
+
 
 if __name__ == '__main__':
-    drone = Drone()
-    drone.camera_capture()
-    drone.takeoff()
-
-    drone.close()
+    pass
