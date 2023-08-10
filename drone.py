3685a4d3c05160a398deac3c9162c19a0503588c
AidenNeale <aidenneale@sky.com>
2023-08-10 17:04:59 +0100

    Updated fliht path

--- drone.py
+++ drone.py
@@ -1,5 +1,6 @@
 import cv2, time
 from cv2 import aruco
+import numpy as np
 from threading import Thread, Event
 
 import robomaster
@@ -22,6 +23,8 @@ class Drone():
         self.pos_x = 0
         self.pos_y = 0
 
+        self.current_image = np.array([])
+
         self.markerID = -1
         self.markerIDPos = {'x': 0, 'y': 0}
 
@@ -39,20 +42,27 @@ class Drone():
         self.thread.start()
         time.sleep(2.5)
 
+
     def camera_thread(self, display):
         while True:
             if self.condition.is_set():
                 break
             img = self.camera.read_cv2_image(strategy="newest")
             img = cv2.flip(img, 0) # flips vertically
+            self.current_image = img
             if display: # If this works, this can be used for computer vision -> ROS? ArUco Tag Detection, etc.
-                markerCorners, markerIds, rejectedCandidates = self.detector_aruco.detectMarkers(img)
+                markerTags, markerIds, rejectedCandidates = self.detector_aruco.detectMarkers(img)
                 if markerIds is not None:
-                    for index, value in enumerate(markerIds):
-                        if value == 10:
-                            self.markerID = value
-                            self.markerIDPos = {'x': self.pos_x, 'y': self.pos_y}
-                aruco.drawDetectedMarkers(img, markerCorners, markerIds)
+                    ids = markerIds.flatten()
+                    for (markerCorner, markerID) in zip(markerTags, ids):
+                        if markerID == 10:
+                            corners = markerCorner.reshape((4, 2))
+                            (topLeft, topRight, bottomRight, bottomLeft) = corners
+                            centreX = round((topLeft[0]+bottomRight[0])/2)
+                            centreY = round((topLeft[1]+bottomRight[1])/2)
+                            self.markerID = markerID
+                            self.markerIDPos = {'x': centreX, 'y': centreY}
+                aruco.drawDetectedMarkers(img, markerTags, markerIds)
                 cv2.imshow('capture', img)
                 cv2.waitKey(1)
 
@@ -69,7 +79,6 @@ class Drone():
         self.drone.close()
         
 
-
     def takeoff(self):
         self.flight.takeoff().wait_for_completed()
         self.flight.up(distance=150).wait_for_completed()  
@@ -80,32 +89,32 @@ class Drone():
 
 
     def head_to_start(self):
-        self.move_forward(position=100)
-        self.move_forward(position=100)
-        self.move_left(position=100)
-        self.move_left(position=100, capture_frame=True)
+        self.move_forward(position=60)
+        self.move_forward(position=60)
+        self.move_forward(position=60)
+        self.move_forward(position=60)
+        self.move_right()
+        self.move_right()
+        self.move_right(position=75)
 
 
     def return_home(self):
-        self.move_backward(position=100)
-        self.move_left(position=100)
-        self.move_left(position=100)
-        self.move_left(position=100)
-        self.move_left(position=100)
-        self.move_backward(position=100)
-        self.move_backward(position=100)
-        self.move_backward(position=100)
-        self.move_backward(position=100)
-        self.move_backward(position=100)
+        return_dist = self.pos_y /2
+        if self.pos_x > 0:
+            self.move_left(self.pos_x)
+        else:
+            self.move_right(-self.pos_x)
+        self.move_backward(return_dist) # Offset introduced to prevent drift causing drone to crash into wall
+        self.move_backward(return_dist) # Offset introduced to prevent drift causing drone to crash into wall
 
 
-    def move_forward(self, position=110):
+    def move_forward(self, position=135):
         self.flight.forward(distance=position).wait_for_completed()
         time.sleep(0.5)
         self.pos_y += position
 
 
-    def move_backward(self, position=110):
+    def move_backward(self, position=135):
         self.flight.backward(distance=position).wait_for_completed()
         time.sleep(0.5)
         self.pos_y -= position
--- drone_flight_path.py
+++ drone_flight_path.py
@@ -1,3 +1,5 @@
+import cv2
+
 from drone import Drone
 
 
@@ -6,37 +8,39 @@ if __name__ == '__main__':
     drone.enable_camera()
     drone.takeoff()
 
-    # Moves forward 200cm || Will make up the new head_to_start function
-    drone.move_forward(position=60)
-    drone.move_forward(position=60)
-    drone.move_forward(position=60)
-
-    # Moves right 350cm || Will make up the new head_to_start function
-    drone.move_right()
-    drone.move_right()
-    drone.move_right(position=75)
+    drone.head_to_start()
 
     with open('flight_path.txt', 'r') as file:
+        found = False
         while True:
             line = file.readline().strip()
-            if drone.markerID == 10:
+            if drone.markerID == 10 and found == False:
                 print("MARKER FOUND!")
-                # break
-            elif line == "forward" or line == "f":
+                found = True
+                break
+            if line == "forward" or line == "f":
                 drone.move_forward()
             elif line == "backward":
                 drone.move_backward()
             elif line == "left":
-                drone.move_left(position=75)
+                drone.move_left(position=125)
             elif line == "right":
-                drone.move_right(position=75)
+                drone.move_right(position=125)
             elif not line:
                 break
             else:
                 print(f"THERE IS AN UNHANDLED INPUT WITHIN FLIGHT PATH: {line}")
 
+    if found == True:
+        #Align over tag
+        #  cv2.imshow("drone", drone.current_image)
+        #  cv2.waitKey(1)
+        print(drone.current_image)
+        print(f"Position of Marker in frame: {drone.markerIDPos}")
 
     print(f"X: {drone.pos_x}, Y: {drone.pos_y}")
+
+    drone.return_home()
 
     drone.land()
     drone.close()
